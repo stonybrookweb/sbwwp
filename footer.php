c20749d181359e5bd87a34bfd5ce30ef8fd6cde0
Stony Brook Web <alana@stonybrookweb.com>
2016-11-25 18:06:30 -0500

    feat: add footer

--- footer.php
+++ footer.php
@@ -13,10 +13,21 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<div class="container">
+  <footer>
+    <div class="row">
+      <hr>
+      <div class="col-xs-12 footer">
+        <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php bloginfo( 'name' ); ?>
+        <a href="https://www.linkedin.com/in/alana-martin-84561233" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        <a href="https://www.facebook.com/stonybrookweb" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="https://github.com/stonybrookweb" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</div>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
