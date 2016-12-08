b938621fcf414deb376710dbd265c4b9a2c5211e
Alana Martin <alana@stonybrookweb.com>
2016-12-08 00:36:40 -0500

    refactor: update blogs page to only show excerpt

--- index.php
+++ index.php
@@ -36,8 +36,12 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
+				if(is_home()){
+				get_template_part( 'template-parts/content', 'blog' );
+				}
+				else {
 				get_template_part( 'template-parts/content', get_post_format() );
-
+				}
 			endwhile;
 
 			the_posts_navigation();
